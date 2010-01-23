ddde063134b44cf3ff2d4765aae706c094c9aabf
Yuriy Brun <ybrun@softarch2.usc.edu>
2010-01-23 15:54:15 -0800

    Added the SweetCode.java file with some text

---
+++ SweetCode.java
@@ -0,0 +1,13 @@
+hello
+This file will contain some text.
+Here is a list.
+1. Item 1
+2. Item 2
+3. Item 3
+4. Item 4
+
+There are many more items that I could create, but I won't for now.
+
+This is not a real java class, it's really just a fake one.  
+
+Oooh, pretty colors.
