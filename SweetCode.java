ed6852c134d60b2aa8d5bb577be122899c11c997
Yuriy Brun <ybrun@softarch2.usc.edu>
2010-02-06 22:26:45 -0800

    added a nonconflicting line at the end

--- SweetCode.java
+++ SweetCode.java
@@ -12,3 +12,5 @@ There are many more items that I could create, but I won't for now.
 This is not a real java class, it's really just a fake one.  
 
 Oooh, pretty colors.
+
+And now I want to add a bunch more lines that actually don't conflict with anyone.
